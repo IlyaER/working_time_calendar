a3c1881c5fc70ab209eeeb2a5d74d9cdb0943d38
IlyaER <16943792+IlyaER@users.noreply.github.com>
2022-10-21 12:55:12 +0300

    some holiday logic corrected

--- Calendar.py
+++ Calendar.py
@@ -40,32 +40,51 @@ class Calendar:
 
         self.c = calendar.LocaleTextCalendar(locale=self.locale)
 
-        self.holidays = [(1, 1), (1, 2), (1, 3), (1, 4),
-                         (1, 5), (1, 6), (1, 7), (1, 8),
-                         (2, 23), (3, 8), (5, 1), (5, 9),
-                         (6, 12), (11, 4)]
+        # month, day, is_transferrable (defines if holiday is transferred when concurs with weekend),
+        # name
+        self.holidays = [(1, 1, 0, "Новогодние каникулы"), (1, 2, 0), (1, 3, 0), (1, 4, 0),
+                         (1, 5, 0), (1, 6, 0), (1, 7, 0), (1, 8, 0),
+                         (2, 23, 1), (3, 8, 1), (5, 1, 1), (5, 9, 1),
+                         (6, 12, 1), (11, 4, 1)]
+
+        self.weekends = set()
 
         self.holiday_transfer = [(2023, )]
 
     def is_holiday(self, day) -> Color:
+        """
+        Checks if a day is in holiday list and returns corresponding color for the day type.
+        Returns black color if not.
+        If it is a transferable holiday and concurs with a weekend, it marks next day as weekend.
+        :param day:
+        :return:
+        """
         year_day, month_day, day, week_day = day
-        if (month_day, day) in self.holidays:
-            return HexColor(0x8B0000)
+        if (month_day, day) in [(holiday[0], holiday[1]) for holiday in self.holidays if holiday[2]]:
+            if week_day > 4:
+                target_day = day + 1
+                while (month_day, target_day) in self.weekends:
+                    target_day += 1
+                self.weekends.add((month_day, target_day))
+                print(sorted(list(self.weekends)))
+            return HexColor(0x990000)
+        if (month_day, day) in self.weekends:
+            return red
         return black
 
     def render_day(self, x: int, y: int, day: tuple, month: int):
         # (2023, 12, 30, 4)
 
         #year_day, month_day, day, week_day = day
-        color = self.is_holiday(day)
-        self.pdf.setFillColor(color)
+
 
         if day[1] != month:
             self.pdf.setFillColor(grey)
-
+        else:
+            color = self.is_holiday(day)
+            self.pdf.setFillColor(color)
         self.pdf.drawRightString(x, y, str(day[2]))
         self.pdf.setFillColor(black)
-        #if self
 
     def render_week(self, x: int, y: int, month: int):
         for i in range(0, 7):
@@ -84,9 +103,9 @@ class Calendar:
 
 
     def render_month(self, x: int, y: int, month: int) -> None:
-        print(x / mm, y / mm)
-        print(self.cell_size / mm)
-        self.pdf.drawString(x, y, calendar.month_name[month])
+        print(f"Month start points: {x / mm, y / mm}")
+        print(self.cell_size / mm, month)
+        self.pdf.drawCentredString(x + self.month_width / 2, y, calendar.month_name[month])
         y += self.cell_size
 
         self.render_week(x, y, month)
@@ -99,15 +118,20 @@ class Calendar:
         self.pdf.setFont(self.font, self.font_size)
 
         # there are 31 cell and 7 in every month in case 4 months per line plus 3 empty cells as border
-        print(self.month_width / mm)
-        print(self.month_height / mm)
+        print(f"Month width: {self.month_width / mm}")
+        print(f"Month height: {self.month_height / mm}")
         #y = self.top_margin + 16 * 2
         for i in range(12):
+            month = i + 1
             # print(f"Месяц {i}, отступ вниз {i % 3}, отступ вбок {i // 3}")
+
+            # populate weekends
+            [self.weekends.add((day[1], day[2])) for day in self.c.itermonthdays4(self.year, month) if day[3] in (5, 6)]
+
             self.render_month(
                 self.left_margin + (self.month_width + self.cell_size) * (i // 3),
                 y + self.month_height * (i % 3),
-                i + 1
+                month
             )
             #self.pdf.drawString(self.left_margin + month_width * (i // 3), y + month_height * (i % 3), calendar.month_name[i + 1])
 
@@ -117,7 +141,8 @@ class Calendar:
         locale.setlocale(locale.LC_ALL, self.locale)
 
 
-        print(self.c.formatmonth(2023, 1))
+
+        #print(self.c.formatmonth(2023, 1))
 
         output = self.year
         # pdf = canvas.Canvas('myfile.pdf', pagesize=self.page_size, bottomup=False)
@@ -125,7 +150,7 @@ class Calendar:
 
         # width_mm = int(width/72*25.4)
         # print(width_mm)
-        print(self.width / mm, self.height / mm)
+        print(f"Page size: {self.width / mm, self.height / mm}")
         fonts = ("DejaVuSans", "Calibri", "CalibriB", "CalibriI", "CalibriL", "CalibriLI", "CalibriZ")
         for font in fonts:
             pdfmetrics.registerFont(TTFont(font, font+".ttf"))
@@ -141,9 +166,9 @@ class Calendar:
         # pdf.drawRightString
 
         # not sure if that calculation is necessary
-        face = pdfmetrics.getFont('CalibriB').face
-        string_height = (face.ascent - face.descent) / 1000 * self.font_size
-        print(string_height)
+        #face = pdfmetrics.getFont('CalibriB').face
+        #string_height = (face.ascent - face.descent) / 1000 * self.font_size
+        #print(string_height)
 
         # pdf.drawString(10, 20, "Календарь")  # , direction='RTL')
         # # one symbol's width is 9, space's is 4
